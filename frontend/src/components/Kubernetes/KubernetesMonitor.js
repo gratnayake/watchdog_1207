285337b252656bb27edd780ecaaf071e092673d6
gratnayake <gratnayake@tsunamitme.com>
2025-07-30 09:05:34 +0530

    Kube update4

--- frontend/src/components/Kubernetes/KubernetesMonitor.js
+++ frontend/src/components/Kubernetes/KubernetesMonitor.js
@@ -78,7 +78,7 @@ const EnhancedKubernetesMonitor = () => {
     if (autoRefresh) {
       interval = setInterval(() => {
         loadEnhancedPods();
-      }, 30000); // Refresh every 30 seconds
+      }, 10000); // Refresh every 30 seconds
     }
     return () => {
       if (interval) clearInterval(interval);
